0ae58010c89b42d5b51585100e6369490d33b5cf
Deborah Yscava <deborahly@gmail.com>
2023-04-07 13:44:38 -0400

    implemented employees edit

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -15,4 +15,28 @@ class EmployeesController < ApplicationController
     end
     redirect_to employees_url
   end
+
+  def edit
+    @element = Employee.find(params[:id])
+  end
+
+  def update
+    ActiveRecord::Base.transaction do
+      @element = Employee.find(params[:id])
+    
+      address = Address.find_or_create_by!(street_address: employee_params[:street_address], city: employee_params[:city], postal_code: employee_params[:postal_code])
+
+      response = @element.update!(address_id: address.id, email: employee_params[:email], phone: employee_params[:phone])
+    end
+
+    if response
+      redirect_to(@element)
+    else
+      render "edit"
+    end
+  end
+
+  def employee_params
+    params.require(:employee).permit(:email, :phone, :street_address, :city, :postal_code)
+  end
 end
