740d867a274c9fe8e13b78ee0d03d31385b211cf
Deborah Yscava <deborahly@gmail.com>
2023-04-07 17:38:26 -0400

    implemented create new employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -7,6 +7,43 @@ class EmployeesController < ApplicationController
     @element = Employee.find(params[:id])
   end
 
+  def new
+    render
+  end
+
+  def create
+    m = ''
+    transaction_succeeded = false
+
+    ActiveRecord::Base.transaction do
+      user = User.where(email: employee_params[:user_email]).first
+      if !user 
+        m = 'User not found.'
+        raise ActiveRecord::Rollback
+      end
+
+      address = Address.find_or_create_by!(street_address: employee_params[:street_address], city: employee_params[:city], postal_code: employee_params[:postal_code])
+
+      employee = Employee.where(user_id: user.id).first
+      if employee
+        m = 'User email already in use.'
+        raise ActiveRecord::Rollback
+      end
+
+      response = Employee.create!(user_id: user.id, address_id: address.id, email: employee_params[:email], phone: employee_params[:phone])
+
+      transaction_succeeded = true
+    end
+
+    if transaction_succeeded
+      flash[:notice] = "Success."
+      redirect_to employees_url
+    else
+      flash[:notice] = m
+      redirect_to new_employee_path
+    end
+  end
+
   def destroy
     if Employee.delete(params[:id]) > 0
       flash[:notice] = "Successfully deleted."
@@ -37,6 +74,6 @@ class EmployeesController < ApplicationController
   end
 
   def employee_params
-    params.require(:employee).permit(:email, :phone, :street_address, :city, :postal_code)
+    params.require(:employee).permit(:user_email, :email, :phone, :street_address, :city, :postal_code)
   end
 end
