6f0a707eca817174161e74a55b149f7f1cdc8c47
Deborah Yscava <deborahly@gmail.com>
2023-04-08 15:02:00 -0400

    corrected controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -17,6 +17,7 @@ class EmployeesController < ApplicationController
 
     ActiveRecord::Base.transaction do
       user = User.where(email: employee_params[:user_email]).first
+      
       if !user 
         m = 'User not found.'
         raise ActiveRecord::Rollback
@@ -25,18 +26,24 @@ class EmployeesController < ApplicationController
       address = Address.find_or_create_by!(street_address: employee_params[:street_address], city: employee_params[:city], postal_code: employee_params[:postal_code])
 
       employee = Employee.where(user_id: user.id).first
+      
       if employee
         m = 'User email already in use.'
         raise ActiveRecord::Rollback
       end
 
-      response = Employee.create!(user_id: user.id, address_id: address.id, email: employee_params[:email], phone: employee_params[:phone])
+      response = Employee.create(user_id: user.id, address_id: address.id, email: employee_params[:email], phone: employee_params[:phone])
+      
+      if !response
+        m = 'Failed to create employee.'
+        raise ActiveRecord::Rollback
+      end
 
       transaction_succeeded = true
     end
 
     if transaction_succeeded
-      flash[:notice] = "Success."
+      flash[:notice] = "Employee created!"
       redirect_to employees_url
     else
       flash[:notice] = m
@@ -58,18 +65,30 @@ class EmployeesController < ApplicationController
   end
 
   def update
+    m = ''
+    transaction_succeeded = false
+
     ActiveRecord::Base.transaction do
       @element = Employee.find(params[:id])
     
       address = Address.find_or_create_by!(street_address: employee_params[:street_address], city: employee_params[:city], postal_code: employee_params[:postal_code])
 
-      response = @element.update!(address_id: address.id, email: employee_params[:email], phone: employee_params[:phone])
+      response = @element.update(address_id: address.id, email: employee_params[:email], phone: employee_params[:phone])
+
+      if !response
+        m = 'Failed to update employee.'
+        raise ActiveRecord::Rollback
+      end
+
+      transaction_succeeded = true
     end
 
-    if response
+    if transaction_succeeded
+      flash[:notice] = "Employee updated!"
       redirect_to(@element)
     else
-      render "edit"
+      flash[:notice] = m
+      redirect_to(@element)
     end
   end
 
