292386618e9c978bb3213baa80a35381677767d7
Deborah Yscava <deborahly@gmail.com>
2023-04-10 13:37:28 -0400

    restaurant_rating includes nil

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
   belongs_to :restaurant
   belongs_to :customer
   belongs_to :order_status
-  validates :restaurant_rating, inclusion: { in: [1, 2, 3, 4, 5]}
+  validates :restaurant_rating, inclusion: { in: [1, 2, 3, 4, 5, nil]}
 end
