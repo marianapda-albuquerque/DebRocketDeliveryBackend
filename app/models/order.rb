d8c00cf2e4fc9a22f7e1308580a28539503e6214
Deborah Yscava <deborahly@gmail.com>
2023-04-14 19:55:04 -0400

    added more relations

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,9 +2,14 @@ class Order < ApplicationRecord
   belongs_to :restaurant
   belongs_to :customer
   belongs_to :order_status
-  belongs_to :courier
+  belongs_to :courier, optional: true
+  
   has_many :product_orders
+  has_many :products, through: :product_orders
+  has_one :user, through: :customer
+  has_one :address, through: :customer
+  
   validates_associated :product_orders
   validates :restaurant_rating, inclusion: { in: [1, 2, 3, 4, 5, nil]}
-  validates :restaurant, :customer, :order_status, presence: true
+  validates :restaurant, :customer, :order_status, presence: true                   
 end
