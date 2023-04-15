5623ca9a27addd9de11c0b3ea2cf2fcbdddbfaf0
Deborah Yscava <deborahly@gmail.com>
2023-04-14 21:46:41 -0400

    refactored

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -22,35 +22,7 @@ class Api::OrdersController < ApplicationController
         response = []
         
         @orders.each {|order|
-            restaurant = Restaurant.find(order.restaurant_id)
-            customer = Customer.find(order.customer_id)
-            courier = Courier.find_by_id(order.courier_id)
-            product_orders = ProductOrder.where(order_id: order.id)
-            products = []
-            product_orders.each {|product_order| 
-                product_order_formatted = {
-                    product_id: product_order.product.id, 
-                    product_name: product_order.product.name, 
-                    quantity: product_order.product_quantity, 
-                    unity_cost: product_order.product.cost, 
-                    total_cost: (product_order.product_quantity * product_order.product.cost)
-                }
-                products.push(product_order_formatted)
-            }
-
-            order_formatted = { 
-                id: restaurant.id, 
-                customer_id: customer.id, 
-                customer_name: customer.user.name, 
-                customer_address: customer.address.street_address, 
-                restaurant_name: restaurant.name, 
-                restaurant_address: restaurant.address.street_address, 
-                courier_id: courier ? courier.id : "", 
-                courier_name: courier ? courier.user.name : "", 
-                status: order.order_status.name,
-                products: products
-            } 
-
+            order_formatted = format_order(order)
             response.push(order_formatted)
         }
 
